7297c53004c4649f9dcb6d0f4fc11bfd46b2bb7f
BeshoBesho <104646429+BeshoBesho@users.noreply.github.com>
2022-05-13 14:59:25 +0200

    ejhsrtn

---
+++ Actions/AddCircAction (2).h
@@ -0,0 +1,23 @@
+#ifndef ADD_CIRC_ACTION_H
+#define ADD_CIRC_ACTION_H
+
+#include "Action.h"
+
+//Add Circle Action class
+class AddCircAction : public Action
+{
+private:
+	Point P1, P2; //Center and Radius
+	GfxInfo CircGfxInfo;
+public:
+	AddCircAction(ApplicationManager* pApp);
+
+	//Reads circle parameters
+	virtual void ReadActionParameters();
+
+	//Add circle to the ApplicationManager
+	virtual void Execute();
+
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main()
 
 	}while(ActType != EXIT);
 	
-
+	//aflknlcd
 	
 	return 0;
 }
